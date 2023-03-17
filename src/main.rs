b5ba5b1598d248225b4243c5b688f51e25d6dd4c
brianxk <brianxk@proton.me>
2023-03-17 13:16:13 -0400

    Added new() function for default board initialization

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 fn main() {
+    let game = model::Game::new();
 
+    println!("{:?}", game);
 }
 
 mod model {
     const BOARD_DIMENSION: usize = 4;
 
-    struct Game {
+    #[derive(Debug)]
+    pub struct Game {
         board: [[u32; BOARD_DIMENSION]; BOARD_DIMENSION],
     }
+
+    impl Game {
+        pub fn new() -> Game {
+            Game {
+                board: [[0; BOARD_DIMENSION]; BOARD_DIMENSION],
+            }
+        }
+    }
 }
+
