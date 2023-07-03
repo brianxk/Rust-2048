ea797072216de8a6f2e4520f63bc614bb455e93e
brianxk <brianxk@proton.me>
2023-07-03 18:51:05 -0400

    pre merge commit

--- dist/rust-2048-989af99fb73cd7a2.js
+++ dist/rust-2048-989af99fb73cd7a2.js
@@ -841,7 +841,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('rust-2048-147201239eb4aaeb_bg.wasm', import.meta.url);
+        input = new URL('rust-2048-989af99fb73cd7a2_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
