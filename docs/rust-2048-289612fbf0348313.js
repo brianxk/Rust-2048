77aebcd6956031c20f5e732858d3cda66008e595
brianxk <brianxk@proton.me>
2023-07-12 18:41:21 -0400

    Experimenting with animation durations

--- docs/rust-2048-289612fbf0348313.js
+++ docs/rust-2048-289612fbf0348313.js
@@ -880,7 +880,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('rust-2048-e559b6b341b45c79_bg.wasm', import.meta.url);
+        input = new URL('rust-2048-289612fbf0348313_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
